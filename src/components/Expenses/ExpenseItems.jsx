f0587286e639ba66d7b83c50afb7d17145693139
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2024-02-27 10:50:21 +0530

    event listener using onclick

--- src/components/Expenses/ExpenseItems.jsx
+++ src/components/Expenses/ExpenseItems.jsx
@@ -6,15 +6,21 @@ import Card from '../UI/Card'
 
 const ExpenseItems = (props) => {
 
+  //adding event listener using react
+  const clickHandler = ()=>{
+   console.log('clicked!!!!')      
+  }
+
    
   return (
-   
+ 
    <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
       <h2>{props.id}</h2>
       <h2>{props.location}</h2>
       <ExpenseDetails title={props.title} price={props.price}/>
+      <button onClick={clickHandler}>Changed title</button>
       </div>
 
     </Card>
