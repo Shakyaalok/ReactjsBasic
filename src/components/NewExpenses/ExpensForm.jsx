e096dfa64446adc9d11e378c13fdcafbf6165749
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2024-02-27 14:38:35 +0530

    important thing abount states in Object

--- src/components/NewExpenses/ExpensForm.jsx
+++ src/components/NewExpenses/ExpensForm.jsx
@@ -7,6 +7,52 @@ const ExpenseForm = () => {
      const [place,setPlace] = useState('');
      const [date,setDate] = useState('');
 
+/*
+    const [userInput,setUserInput] = useState({
+        title:'',
+        price:'',
+        place:'',
+        date:''
+    })
+
+    
+    const titleHandler  = (e)=>{
+        // setUserInput({
+        //     ...userInput,
+        //     title:e.target.value
+        // })
+        // if we have lot of state the we can get the older state or outdated state or incorrect state so 
+        // using below method then react will gurantee that snapshot state will be the latest
+
+        setUserInput((prevState)=>{
+            return {...prevState,title:e.target.value}
+        })
+     }
+
+
+     const placeHandler  = (e)=>{
+        // setUserInput({
+        //     ...userInput,
+        //     place:e.target.value
+        // })
+     }
+
+     const priceHandler  = (e)=>{
+        // setUserInput({
+        //     ...userInput,
+        //     price:e.target.value
+        // })
+     }
+
+
+     const dateHandler  = (e)=>{
+        setUserInput({
+            ...userInput,
+            date:e.target.value
+        })
+     }
+*/
+
     const submitFormHandler = () =>{
         console.log(title,price,place,date);
      
